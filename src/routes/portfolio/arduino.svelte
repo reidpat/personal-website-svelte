b08aa07538dfe672eeae1ce3ba7641be9a8794c2
reidpat <61036489+reidpat@users.noreply.github.com>
2022-12-28 15:26:07 -0800

    Adding links to Unity ML project

--- src/routes/portfolio/arduino.svelte
+++ src/routes/portfolio/arduino.svelte
@@ -38,7 +38,7 @@
         </ul>
 
 		<h2 class="text-2xl bold divider">Examples</h2>
-        <p>As this project took place several years ago now, I unfortunately have less videos and demonstrations recorded than I would like. If you would like to see updated examples implemented virtually with Unity, you can find those on my <a href="/portfolio/unityml"> Unity project page</a>.</p>
+        <p>As this project took place several years ago now, I unfortunately have less videos and demonstrations recorded than I would like. If you would like to see updated examples implemented virtually with Unity, you can find those on my <a class="gradient-text" href="/portfolio/unityml"> Unity project page</a>.</p>
 		<iframe width="560" height="315" src="https://www.youtube.com/embed/bx9dvp-c9IY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         <iframe width="560" height="315" src="https://www.youtube.com/embed/jnnQy7Nw3Q0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 		
