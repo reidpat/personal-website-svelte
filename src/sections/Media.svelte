bde33f25ce7ea81e6988569e88df83e652f1ef5b
reidpat <61036489+reidpat@users.noreply.github.com>
2023-03-28 09:32:39 -0700

    Using a new gallery for videos - works?

--- src/sections/Media.svelte
+++ src/sections/Media.svelte
@@ -1,26 +1,39 @@
 <script>
-	import Lazy from 'svelte-lazy';
+	import '@splidejs/splide/css';
+	import { Splide, SplideSlide } from '@splidejs/svelte-splide';
+	import { Video } from '@splidejs/splide-extension-video';
+	import '@splidejs/splide-extension-video/dist/css/splide-extension-video.min.css';
 
-	let showLoading = "relative";
-	function iframeLoad(){
-		showLoading = "none";
-	}
+	const videos = [ 'R1Z-T8IQxbQ','u2Q-nMUDmvU', 'cqaQNkmpyQ4' ];
 </script>
 
 <div id="media" class="hero section min-h-screen bg-base-200 flex justify-center">
 	<div class="hero-content flex align-middle justify-center text-center flex-col ">
 		<h1 class="text-5xl font-bold">Media</h1>
-		<div class="flex justify-center w-full py-2 gap-2">
-			<a href="#media-item1"
-				><button class="btn btn-circle glow-on-hover bright">1</button></a
-			>
-			<a href="#media-item2"
-				><button class="btn btn-circle glow-on-hover bright">2</button></a
-			>
-		</div>
-		<div class="carousel w-full">
+		<Splide
+			options={{ rewind: true, heightRatio: 0.5625 }}
+			extensions={{ Video }}
+			aria-labelledby="video-example-heading"
+		>
+			{#each videos as id, index}
+				<SplideSlide data-splide-youtube={id}>
+					<img
+						src={`https://i3.ytimg.com/vi/${id}/maxresdefault.jpg`}
+						alt={`YouTube Sample ${index + 1}`}
+					/>
+				</SplideSlide>
+			{/each}
+		</Splide>
+		<!-- <div class="flex justify-center w-full py-2 gap-2">
+			<a href="#media-item1"><button class="btn btn-circle glow-on-hover bright">1</button></a>
+			<a href="#media-item2"><button class="btn btn-circle glow-on-hover bright">2</button></a>
+		</div> -->
+		<!-- <div class="carousel w-full">
 			<div class="iframe-container carousel-item w-full" id="media-item1">
-				<progress class="progress progress-secondary w-80" style="display: {showLoading}; left: 50%; top: 50%"></progress>
+				<progress
+					class="progress progress-secondary w-80"
+					style="display: {showLoading}; left: 50%; top: 50%"
+				/>
 				<Lazy>
 					<iframe
 						src="https://www.youtube.com/embed/R1Z-T8IQxbQ"
@@ -44,21 +57,21 @@
 					/>
 				</Lazy>
 			</div>
-		</div>
+		</div> -->
 	</div>
 </div>
 
 <style>
 	.iframe-container {
-		position: relative;
-		overflow: hidden;
-		width: 90vw;
+		/* position: relative;
+		overflow: hidden; */
+		/* width: 90vw; */
 		max-width: 1200px;
 		height: 50vw;
 		min-height: 200px;
 		max-height: 600px;
 	}
-	iframe {
+	/* iframe {
 		position: absolute;
 		top: 0;
 		left: 10%;
@@ -66,5 +79,5 @@
 		right: 10%;
 		width: 80%;
 		height: 100%;
-	}
+	} */
 </style>
